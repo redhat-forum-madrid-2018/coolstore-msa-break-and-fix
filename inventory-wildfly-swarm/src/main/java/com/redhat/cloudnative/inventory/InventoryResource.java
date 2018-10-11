966fc95b6b1a40568d61ca30198d60905e3a8f05
rmarting <rmarting@redhat.com>
2018-10-11 23:04:37 +0200

    Break and Fix at Inventory Microservice

--- inventory-wildfly-swarm/src/main/java/com/redhat/cloudnative/inventory/InventoryResource.java
+++ inventory-wildfly-swarm/src/main/java/com/redhat/cloudnative/inventory/InventoryResource.java
@@ -1,6 +1,7 @@
 package com.redhat.cloudnative.inventory;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.ws.rs.GET;
@@ -8,21 +9,27 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import org.wildfly.swarm.health.Health;
-import org.wildfly.swarm.health.HealthStatus;
-import java.util.Date;
+
+import com.redhat.cloudnative.inventory.breakfix.service.BreakFixService;
 
 @Path("/")
 @ApplicationScoped
 public class InventoryResource {
-    @PersistenceContext(unitName = "InventoryPU")
+
+	@PersistenceContext(unitName = "InventoryPU")
     private EntityManager em;
+	
+	@Inject
+	private BreakFixService breakFixService;
 
     @GET
     @Path("/api/inventory/{itemId}")
     @Produces(MediaType.APPLICATION_JSON)
     public Inventory getAvailability(@PathParam("itemId") String itemId) {
         Inventory inventory = em.find(Inventory.class, itemId);
+        
+        // Break & Fix
+        this.breakFixService.process();
 
         if (inventory == null) {
             inventory = new Inventory();
