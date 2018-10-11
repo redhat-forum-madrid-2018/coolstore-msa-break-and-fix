78d8ba3ba0b1f295c7a24cba9e778ea6826d0336
rmarting <rmarting@redhat.com>
2018-10-11 23:47:48 +0200

    Break and Fix for Spring Boot microservice

--- catalog-spring-boot/src/main/java/com/redhat/cloudnative/catalog/CatalogController.java
+++ catalog-spring-boot/src/main/java/com/redhat/cloudnative/catalog/CatalogController.java
@@ -12,17 +12,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.redhat.cloudnative.catalog.breakfix.service.BreakFixService;
+
 @Controller
 @RequestMapping(value = "/api/catalog")
 public class CatalogController {
 
 	@Autowired
     private ProductRepository repository;
+	
+	@Autowired
+	private BreakFixService breakFixService;
 
     @ResponseBody
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Product> getAll() {
         Spliterator<Product> products = repository.findAll().spliterator();
+        
+        breakFixService.process();
+        
         return StreamSupport.stream(products, false).collect(Collectors.toList());
     }
     
