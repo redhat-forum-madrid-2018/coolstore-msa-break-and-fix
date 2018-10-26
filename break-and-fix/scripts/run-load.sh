b94bfccc0b92515b3cf59f43fe3d9944c9eecf2a
Roman Martin Gil <rmarting@redhat.com>
2018-10-26 11:18:12 +0200

    Added load for Web UI

--- break-and-fix/scripts/run-load.sh
+++ break-and-fix/scripts/run-load.sh
@@ -3,13 +3,18 @@
 oc delete pod siege-load-gateway --force
 oc delete pod siege-load-catalog --force
 oc delete pod siege-load-inventory --force
+oc delete pod siege-load-web-ui --force
+
 sleep 10
 
 oc run siege-load-gateway -n coolstore --restart='OnFailure' --image=siamaksade/siege \
-  -- -c4 -d2 -t10M -v http://gateway-coolstore.$(minishift ip).nip.io/api/products
+  -- -c4 -d6 -t60M -v http://gateway-coolstore.$(minishift ip).nip.io/api/products
 
 oc run siege-load-catalog -n coolstore --restart='OnFailure' --image=siamaksade/siege \
-  -- -c4 -d2 -t10M -v http://catalog-coolstore.$(minishift ip).nip.io/api/catalog
+  -- -c4 -d6 -t60M -v http://catalog-coolstore.$(minishift ip).nip.io/api/catalog
 
 oc run siege-load-inventory -n coolstore --restart='OnFailure' --image=siamaksade/siege \
-  -- -c4 -d2 -t10M -v http://inventory-coolstore.$(minishift ip).nip.io/api/inventory/329299
+  -- -c4 -d6 -t60M -v http://inventory-coolstore.$(minishift ip).nip.io/api/inventory/329299
+
+oc run siege-load-web-ui -n coolstore --restart='OnFailure' --image=siamaksade/siege \
+  -- -c4 -d6 -t60M -v http://web-ui-coolstore.$(minishift ip).nip.io/
