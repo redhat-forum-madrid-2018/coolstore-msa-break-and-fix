142fea09cb2c862ea735507a085b8e47db9c0cca
rhforum2018 <rhforum2018@kiuran.localdomain>
2018-10-29 16:38:18 +0100

    Added more loaders

--- break-and-fix/scripts/run-load.sh
+++ break-and-fix/scripts/run-load.sh
@@ -1,19 +1,23 @@
 #!/bin/sh
 
-oc delete pod siege-load-gateway --force
-oc delete pod siege-load-catalog --force
-oc delete pod siege-load-inventory --force
-oc delete pod siege-load-web-ui --force
+oc delete pod siege-load-gateway-ingress -n coolstore --force
+oc delete pod siege-load-gateway -n coolstore --force
+oc delete pod siege-load-catalog -n coolstore --force
+oc delete pod siege-load-inventory -n coolstore --force
+oc delete pod siege-load-web-ui -n coolstore --force
 
 sleep 10
 
 # Siege Configuration
 # Info: https://www.joedog.org/siege-manual/
-CONCURRENT_USERS="-c4"
+CONCURRENT_USERS="-c2"
 # Random delay of seconds between requests
-DELAY="-d6"
+DELAY="-d10"
 # Period of time to run the test
 TIME="-t60M"
+
+oc run siege-load-gateway-ingress -n coolstore --restart='OnFailure' --image=siamaksade/siege \
+  -- $CONCURRENT_USERS $DELAY $TIME -v http://istio-ingressgateway-istio-system.$(minishift ip).nip.io/api/products
 
 oc run siege-load-gateway -n coolstore --restart='OnFailure' --image=siamaksade/siege \
   -- $CONCURRENT_USERS $DELAY $TIME -v http://gateway-coolstore.$(minishift ip).nip.io/api/products
@@ -26,3 +30,4 @@ oc run siege-load-inventory -n coolstore --restart='OnFailure' --image=siamaksad
 
 oc run siege-load-web-ui -n coolstore --restart='OnFailure' --image=siamaksade/siege \
   -- $CONCURRENT_USERS $DELAY $TIME -v http://web-ui-coolstore.$(minishift ip).nip.io/
+
